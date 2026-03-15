c14ee4be1acbc93320adfc024b535b8a0351996f
KKT <kimgt2828@gmail.com>
2026-03-16 01:08:47 +0900

    회원가입 로직 구현

--- src/main/java/com/likelion/animalface/domain/user/service/UserService.java
+++ src/main/java/com/likelion/animalface/domain/user/service/UserService.java
@@ -1,13 +1,23 @@
 package com.likelion.animalface.domain.user.service;
 
 import com.likelion.animalface.domain.user.dto.SignupReq;
-import org.jspecify.annotations.Nullable;
+import com.likelion.animalface.domain.user.repositry.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 @Service
 public class UserService {
 
+    UserRepository userRepository;
+    PasswordEncoder passwordEncoder;
+
+    @Transactional
     public void signup(SignupReq req) {
+        userRepository.save(req.to(passwordEncoder.encode(req.password())));
     }
 
     public String getUsername(String phone) {
