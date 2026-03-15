de6670b8c460ccedcb4bdba13281cc87e4fb8c5f
KKT <kimgt2828@gmail.com>
2026-03-15 19:53:00 +0900

    init: initial setting

---
+++ src/test/java/com/likelion/animalface/AnimalFaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.animalface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalFaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
